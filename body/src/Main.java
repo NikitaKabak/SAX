04528c8467a154604e1e93e2e6c60a690033e68c
nikita Kabak <nzkabak@gmail.com>
2018-04-07 19:31:46 +0300

    Add new class SAXPars

--- body/src/Main.java
+++ body/src/Main.java
@@ -1,5 +1,21 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+
 public class Main {
-    public static  void main(String[] args){
+    public static  void main(String[] args) throws ParserConfigurationException, SAXException {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = factory.newSAXParser();
+        SAXPars pars = new SAXPars();
+        pars.startDocument();
+        try {
+            parser.parse("books.xml",pars);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- body/src/SAXPars.java
+++ body/src/SAXPars.java
@@ -1,2 +1,9 @@
-public class SAXPars {
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXPars extends DefaultHandler{
+
+    public  void startDocument(){
+        System.out.println(" Start parse XML");
+    }
+
 }
